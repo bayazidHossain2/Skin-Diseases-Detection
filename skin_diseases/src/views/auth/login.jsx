9155bab48190d7f15dc3b9447e7baf4d4be359ce
dirsty22 <dirstykhatun.190122@s.pust.ac.bd>
2023-09-20 21:00:00 +0600

    Text Correction and color change

--- skin_diseases/src/views/auth/login.jsx
+++ skin_diseases/src/views/auth/login.jsx
@@ -26,11 +26,11 @@ export default function Login() {
       }
       <div className="">
         <form>
-          <h2 className=' font-bold text-xl text-blue-950'>Sign In :</h2>
+          <h2 className=' font-bold text-xl text-black'>Sign In :</h2>
           <div className=''>
             <label className="block text-sm font-medium text-slate-700">Email</label>
             <div className="mt-1">
-              <input ref={emailRef} type="email" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your email." onChange={({ value }) => {
+              <input ref={emailRef} type="email" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your email." onChange={({ value }) => {
                 console.log(value);
               }} />
             </div>
@@ -38,19 +38,15 @@ export default function Login() {
           <div className="mt-6">
             <label className="block text-sm font-medium text-slate-700">Password</label>
             <div className="mt-1">
-              <input ref={passwordRef} type="password" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your password." />
+              <input ref={passwordRef} type="password" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your password." />
             </div>
-          </div>
-          <div className="mt-1 text-right">
-            <Link className=" hover:text-blue-500 pt-2.5 text-sm leading-5 rounded-md font-semibold text-blue-300 cursor-pointer" to="/mail-varify">Goto mail varification.
-            </Link>
           </div>
           <div className="mt-1 text-right">
             <Link className=" hover:text-blue-500 pt-2.5 text-sm leading-5 rounded-md font-semibold text-blue-300 cursor-pointer" to="/signup">New User? Create an account.
             </Link>
           </div>
           <div className="mt-6 text-right">
-            <div className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white">
+            <div className="bg-orange-700 hover:bg-orange-600 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white">
               Sign in
             </div>
           </div>
--- skin_diseases/src/views/auth/signup.jsx
+++ skin_diseases/src/views/auth/signup.jsx
@@ -5,8 +5,7 @@ import { useStateContext } from '../../contexts/contextProvider';
 export default function Signup() {
   const nameRef = useRef();
   const emailRef = useRef();
-  const sidRef = useRef();
-  const sessionRef = useRef();
+  const DOBRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
 
@@ -28,12 +27,12 @@ export default function Signup() {
           </div>
         }
         <form>
-          <h2 className=' font-bold text-xl text-blue-950'>Registration :</h2>
+          <h2 className=' font-bold text-xl text-black'>Registration :</h2>
           {/* Name  */}
           <div className='inp-component'>
             <label className="block text-sm font-medium text-slate-700">Name</label>
             <div className="mt-1">
-              <input ref={nameRef} type="text" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your Name." />
+              <input ref={nameRef} type="text" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your Name." />
             </div>
           </div>
 
@@ -41,39 +40,28 @@ export default function Signup() {
           <div className='inp-component mt-4'>
             <label className="block text-sm font-medium text-slate-700">Email</label>
             <div className="mt-1">
-              <input ref={emailRef} type="email" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your email." />
+              <input ref={emailRef} type="email" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your email." />
             </div>
           </div>
-
-          {/* Student id and Session */}
-          <div className="flex flex-row space-x-6">
-            {/* Student id  */}
-            <div className='inp-component mt-4'>
-              <label className="block text-sm font-medium text-slate-700">Student id</label>
-              <div className="mt-1">
-                <input ref={sidRef} type="number" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your roll." />
-              </div>
-            </div>
-            {/* Session  */}
-            <div className='inp-component mt-4'>
-              <label className="block text-sm font-medium text-slate-700">Session</label>
-              <div className="mt-1">
-                <input ref={sessionRef} type="text" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="2018-19" />
-              </div>
+          {/* Email  */}
+          <div className='inp-component mt-4'>
+            <label className="block text-sm font-medium text-slate-700">Date of birth: </label>
+            <div className="mt-1">
+              <input ref={DOBRef} type="date" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your email." />
             </div>
           </div>
           {/* password  */}
           <div className="inp-component mt-4">
             <label className="block text-sm font-medium text-slate-700">Password</label>
             <div className="mt-1">
-              <input ref={passwordRef} type="password" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your password." />
+              <input ref={passwordRef} type="password" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your password." />
             </div>
           </div>
           {/* Confirm Password  */}
           <div className="inp-component mt-4">
             <label className="block text-sm font-medium text-slate-700">Confirm Password</label>
             <div className="mt-1">
-              <input ref={confirmPasswordRef} type="password" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your password again." />
+              <input ref={confirmPasswordRef} type="password" className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter your password again." />
             </div>
           </div>
 
@@ -84,13 +72,13 @@ export default function Signup() {
 
           {loading &&
             <div className="mt-1 text-center">
-              <div className=" text-blue-500 pt-2.5 text-sm leading-5 rounded-md font-semibold" >Wait for surver response...
+              <div className=" text-orange-500 pt-2.5 text-sm leading-5 rounded-md font-semibold" >Wait for surver response...
               </div>
             </div>
           }
 
           <div className={loading ? " opacity-50 pointer-events-none cursor-default mt-6 text-right" : "mt-6 text-right"}>
-            <div onClick={submit} className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white">
+            <div className="bg-orange-700 hover:bg-orange-600 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white">
               Sign up
             </div>
           </div>
